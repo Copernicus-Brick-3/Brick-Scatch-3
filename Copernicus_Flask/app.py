3a709c55c580cf6d86c50d3b2292a69eef93055d
PE <pierre.delagrange@hotmail.fr>
2022-05-31 15:29:22 +0200

    Mise a jour app.py

--- Copernicus_Flask/app.py
+++ Copernicus_Flask/app.py
@@ -1,15 +1,12 @@
-import cdsapi
-import datetime
 from flask import Flask
-from netCDF4 import Dataset
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
+
+import datetime
+import cdsapi
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return datetime.datetime.now().strftime("%H:%M:%S")
 
 @app.route("/download")
@@ -20,54 +17,15 @@ def download():
     #Recupere et telecharge en local les donnée sous le format nc
     #TODO: Recuperer des valeur et modifier les valeur present dans cette requetes.
     c.retrieve(
-        'efas-forecast',
+        'cams-global-reanalysis-eac4',
         {
-            'format' : 'netcdf',
+            'date': '2003-01-01/2003-01-01',
+            'format': 'netcdf',                                 
+            'variable': '2m_temperature',
+            'time': '00:00',
         },
         'download.nc')
-        return "download done"
-    
+    return "download done"
 
-@app.route("/convertimg")
-def NcToPng():
-    #recupere le fichier en local
-    data = Dataset('download.nc')
-
-    #different valeur stocker dans le fichier nc
-    lats = data.variables['latitude'][:]
-    lons = data.variables['longitude'][:]
-    time = data.variables['time'][:]
-    #valeur qui differe pour les different theme 
-    t2m = data.variables['t2m'][:]
-    #TODO:Pouvoir faire changer la valeur pour chaque themes donné en parametres
-
-    #Creer une carte avec leur coordonées
-    mp=Basemap(projection='mill',lat_ts=10,llcrnrlon=lons.min(), \
-    urcrnrlon=lons.max(),llcrnrlat=lats.min(),urcrnrlat=lats.max(), \
-    resolution='c')
-    lon, lat = np.meshgrid(lons, lats)
-    x,y = mp(lon, lat)
-
-    #Rajoute les code couleur et 
-    c_scheme = mp.pcolor(x, y,np.squeeze(t2m[0,:,:]),shading='flat',cmap=plt.cm.jet)
-    #Dessine les frontieres avec les different pays
-    mp.drawcoastlines()
-    mp.drawstates()
-    mp.drawcountries()
-    #Rajoute une legende sous forme de barre
-    cbar = mp.colorbar(c_scheme, location = 'right', pad = '10%')
-
-    #TODO:Le titre en en-tete et le nom de l'images doit etre changer avec la date et le themes stocker dans le fichier nc
-    plt.title('TEST')
-    #telecharge un fichier en png
-    plt.savefig('image.png')
-
-@app.route("/img")
-#TODO:Recuperer des images et les envoyer vers mblock
-
-
-@app.after_request
-def add_header(response):
-    #permet de pouvoir faire des requetes http depuis un site web(Cross-origin resource sharing)
-    response.headers['Access-Control-Allow-Origin'] = '*' 
-    return response
+if __name__ == "__main__":
+    app.run()
