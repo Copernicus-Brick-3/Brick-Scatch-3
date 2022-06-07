20a173985118012c7eda7e03f5e5d91beb4c5e8a
PE <pierre.delagrange@hotmail.fr>
2022-06-07 15:47:19 +0200

    Mise a jour app.py -> Gere les parametres date et type

--- Copernicus_Flask/app.py
+++ Copernicus_Flask/app.py
@@ -1,21 +1,66 @@
 from flask import Flask
-
+from flask import request
 import datetime
 import cdsapi
+from zipfile import ZipFile
+
+from ncplot import view
+
+def extractFiles():
+    print("EXTRACT FILES")
+    with ZipFile('download.zip', 'r') as zipObj:
+        listOfFileNames = zipObj.namelist()
+        for fileName in listOfFileNames:
+            # Check filename endswith csv
+            if fileName.endswith('.nc') or fileName.endswith('.grib'):
+                # Extract a single file from zip
+                zipObj.extract(fileName, 'extracted_files')
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return datetime.datetime.now().strftime("%H:%M:%S")
+    
 
 @app.route("/download")
 def download():
+    requestType = request.args.get('request', default='defaultRequest',type=str)
+    day = request.args.get('day', default='01',type=str)
+    month = request.args.get('month', default='01',type=str)
+    year = request.args.get('year', default='01',type=str)
     #connexion a l'api de copernicus
-    c = cdsapi.Client(url='https://ads.atmosphere.copernicus.eu/api/v2', key='5729:f5324f2d-ebed-4932-b892-dea8bca853f3')   # <---- renseigner sa propre clé API
+    #c = cdsapi.Client(url='https://ads.atmosphere.copernicus.eu/api/v2', key='5729:f5324f2d-ebed-4932-b892-dea8bca853f3')   # <---- renseigner sa propre clé API
+    if requestType == 'aerosol':
+        c = cdsapi.Client(url='https://cds.climate.copernicus.eu/api/v2', key='138730:2ca5cd16-2d91-4c9c-995d-3735c28863e7')   # <---- renseigner sa propre clé API
+        c.retrieve(
+            'satellite-aerosol-properties',
+            {
+                'time_aggregation': 'daily_average',
+                'variable': 'dust_aerosol_optical_depth',
+                'sensor_on_satellite': 'iasi_on_metopa',
+                'algorithm': 'ens',
+                'year': year,
+                'month': month,
+                'day': day,
+                'version': 'v1.1',
+                'orbit': 'descending',
+                'format': 'zip',
+            },
+            'download.zip')
+        extractFiles()
+    elif requestType == 'nox':
+        print("TODO")
+    elif requestType == 'melting': 
+        print("TODO")
+    else:
+        return "ERROR ON REQUEST"
+    return "Download finished"
+    
 
-    #Recupere et telecharge en local les donnée sous le format nc
-    #TODO: Recuperer des valeur et modifier les valeur present dans cette requetes.
+@app.route("/test")
+def test():
+    c = cdsapi.Client(url='https://ads.atmosphere.copernicus.eu/api/v2', key='5729:f5324f2d-ebed-4932-b892-dea8bca853f3')
     c.retrieve(
         'cams-global-reanalysis-eac4',
         {
@@ -25,7 +70,12 @@ def download():
             'time': '00:00',
         },
         'download.nc')
-    return "download done"
+    return "Téléchargement terminé"
+
+@app.route("/convert")
+def convert():
+    print("TODO")
+
 
 if __name__ == "__main__":
     app.run()
